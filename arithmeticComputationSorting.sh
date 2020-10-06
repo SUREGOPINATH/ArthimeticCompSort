92bd98320ccf5713ee98ae0725761f34ab6ceb74
SURE GOPINATH <suregopinath@gmail.com>
2020-10-06 16:46:38 +0530

    master code

---
+++ arithmeticComputationSorting.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+##code of master
+echo "welcome"
+
+##code of UserCase1
+read -p "Enter the value1" number1
+read -p "Enter the value1" number2
+read -p "Enter the value1" number3
+
+##code of Computation 1
+
+comp1=$(($number1+$number2
+
+

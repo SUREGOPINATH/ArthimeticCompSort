47718fff4882c5dbad78bfaffdb9a2b533716e0d
SURE GOPINATH <suregopinath@gmail.com>
2020-10-06 17:16:19 +0530

    dictionary is added

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -23,4 +23,15 @@ comp3=$(($number3+($number1/$number2)))
 
 ##code of computation 4
 
-comp=$(($number1%$number2+$number3))
+comp4=$(($number1%$number2+$number3))
+
+##code of storing computation values in dictionaries
+
+declare -A compDictionary
+
+compDictionary['comp1']=$comp1
+compDictionary['comp2']=$comp2
+compDictionary['comp3']=$comp3
+compDictionary['comp4']=$comp4
+
+echo ${compDictionary[@]}
